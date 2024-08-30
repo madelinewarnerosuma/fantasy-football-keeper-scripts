fba5d6386670865818336f9c4d7f6878653281a9
Madeline Warner <madelinewarner16@gmail.com>
2024-08-29 22:54:08 -0500

    messing with ESPN data

--- src/espn_spike_scripts.py
+++ src/espn_spike_scripts.py
@@ -17,7 +17,38 @@ def main():
     initial_response = requests.get(new_url, params=params)
 
     response_json = initial_response.json()
-    print(response_json)
+    # print(response_json)
+    owners_by_year_dictionary, owner_id_to_yearly_stats = {}, {}
+    for year in response_json:
+        owners_by_year_dictionary[year.get('seasonId')] = [{
+            'id': x.get('id'), 'name': f"{x.get('firstName', '')} {x.get('lastName', '')}"
+        } for x in year.get('members', [])]
+
+        for x in year.get('teams', []):
+            if x.get('primaryOwner') not in owner_id_to_yearly_stats:
+                owner_id_to_yearly_stats[x.get('primaryOwner', '')] = {year.get('seasonId', ''): {
+                    'team_name': x.get('name'),
+                    'final_position': x.get('rankCalculatedFinal'),
+                    'points': x.get('points'),
+                    'playoff_seed': x.get('playoffSeed')
+                }}
+            else:
+                owner_id_to_yearly_stats[x.get('primaryOwner')][year.get('seasonId', '')] = {
+                    'team_name': x.get('name'),
+                    'final_position': x.get('rankCalculatedFinal'),
+                    'points': x.get('points'),
+                    'playoff_seed': x.get('playoffSeed')
+                }
+
+    owner_ids_to_names = {y.get('id'): y.get('name') for y in [item for sublist in [x for x in owners_by_year_dictionary.values()] for item in sublist]}
+
+    final_dict = {
+        owner_ids_to_names.get(x, 'Ghost?'): owner_id_to_yearly_stats.get(x) for x in owner_id_to_yearly_stats.keys()
+    }
+
+
+    print("Let's see it!")
+
 
 
 if __name__ == '__main__':
